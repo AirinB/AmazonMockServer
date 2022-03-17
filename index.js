a3c7d40763335bbfd79403bd363997014c92ea63
AirinB <andreeamoraru090@gmail.com>
2022-03-17 14:47:40 +0100

    add mock json files and create the structure of the API

--- index.js
+++ index.js
@@ -4,10 +4,34 @@ const port = 8000
 
 // run server on node .
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+    res.send('Hello World!');
+});
+
+app.get('/christmas', (req, res) => {
+    res.send('Christmas');
+});
+
+app.get('/birthday', (req, res) => {
+    res.send('Birthday');
+});
+
+app.get('/anniversary', (req, res) => {
+    res.send('Anniversary');
+});
+
+app.get('/valentines', (req, res) => {
+    res.send('Valentines');
+});
+
+app.get('/woman', (req, res) => {
+    res.send('Woman');
+});
+
+app.get('/man', (req, res) => {
+    res.send('man');
+});
 
 
 app.listen(port, () => {
-    console.log(`Example app listening on http://localhost:${port}`)
+    console.log(`App running on http://localhost:${port}`)
 })
