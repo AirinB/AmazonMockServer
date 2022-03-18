35f41e82e8f4e79355268f11f3f73562a5a3f52f
AirinB <andreeamoraru090@gmail.com>
2022-03-18 12:02:44 +0100

    add sample project to deploy on vercel

--- index.js
+++ index.js
@@ -1,48 +1,60 @@
-const express = require('express')
-const app = express()
-const port = 8000
+// const express = require('express')
+// const app = express()
+// const port = 8080
+//
+// const getData = (filepath) => {
+//     const jsonData= require(filepath);
+//     return jsonData.results
+// };
+//
+// // run server on node .
+// app.get('/', (req, res) => {
+//     res.send('Hello World!');
+// });
+//
+// app.get('/christmas', (req, res) => {
+//     const result = getData('./API_Json_Responces/search_Christmas.json');
+//     res.send(result);
+// });
+//
+// app.get('/birthday', (req, res) => {
+//     const result = getData('./API_Json_Responces/seach_birthday.json');
+//     res.send(result);
+// });
+//
+// app.get('/anniversary', (req, res) => {
+//     const result = getData('./API_Json_Responces/search_aniversary_gifts.json');
+//     res.send(result);
+// });
+//
+// app.get('/valentines', (req, res) => {
+//     const result = getData('./API_Json_Responces/search_valentines.json');
+//     res.send(result);
+// });
+//
+// app.get('/woman', (req, res) => {
+//     const result = getData('./API_Json_Responces/search_woman_gifts.json');
+//     res.send(result);
+// });
+//
+// app.get('/man', (req, res) => {
+//     const result = getData('./API_Json_Responces/search_man_gifts.json');
+//     res.send(result);
+// });
+//
+//
+// app.listen(port, () => {
+//     console.log(`App running on http://localhost:${port}`)
+// })
 
-const getData = (filepath) => {
-    const jsonData= require(filepath);
-    return jsonData.results
-};
 
-// run server on node .
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+const express = require("express");
+const app = express();
+const product = require("./api/product");
 
-app.get('/christmas', (req, res) => {
-    const result = getData('./API_Json_Responces/search_Christmas.json');
-    res.send(result);
-});
+app.use(express.json({ extended: false }));
 
-app.get('/birthday', (req, res) => {
-    const result = getData('./API_Json_Responces/seach_birthday.json');
-    res.send(result);
-});
+app.use("/api/product", product);
 
-app.get('/anniversary', (req, res) => {
-    const result = getData('./API_Json_Responces/search_aniversary_gifts.json');
-    res.send(result);
-});
-
-app.get('/valentines', (req, res) => {
-    const result = getData('./API_Json_Responces/search_valentines.json');
-    res.send(result);
-});
-
-app.get('/woman', (req, res) => {
-    const result = getData('./API_Json_Responces/search_woman_gifts.json');
-    res.send(result);
-});
-
-app.get('/man', (req, res) => {
-    const result = getData('./API_Json_Responces/search_man_gifts.json');
-    res.send(result);
-});
-
-
-app.listen(port, () => {
-    console.log(`App running on http://localhost:${port}`)
-})
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
