2695e08ca47c6f47dcee0d45e44094e69e44c006
AirinB <andreeamoraru090@gmail.com>
2022-05-16 00:22:54 +0200

    try to get puppeteer work on vercel

--- api/utils/browser.js
+++ api/utils/browser.js
@@ -1,7 +1,12 @@
-const puppeteer = require("puppeteer");
+import chromium from "chrome-aws-lambda";
 
 const getHtml = async (url) => {
-  const browser = await puppeteer.launch();
+  const browser = await chromium.puppeteer.launch({
+    args: [...chromium.args],
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath,
+  });
+
   const page = await browser.newPage();
   await page.goto(url);
   const data = await page.evaluate(() => document.querySelector("*").outerHTML);
